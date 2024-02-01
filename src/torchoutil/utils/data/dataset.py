d6b0d215057244f4dc6baf880b17ede9ae41195c
Labbeti <etienne.labbe31@gmail.com>
2024-02-01 18:06:01 +0100

    Fix: SizedDatasetLike typing and parent classes order.

--- src/torchoutil/utils/data/dataset.py
+++ src/torchoutil/utils/data/dataset.py
@@ -11,15 +11,15 @@ T = TypeVar("T", covariant=True)
 class EmptyDataset(Dataset):
     """Dataset placeholder. Raises StopIteration if __getitem__ is called."""
 
-    def __getitem__(self, index) -> None:
+    def __getitem__(self, idx) -> None:
         raise StopIteration
 
     def __len__(self) -> int:
         return 0
 
 
-class SizedDatasetLike(Protocol, Generic[T]):
-    def __getitem__(self, index) -> T:
+class SizedDatasetLike(Generic[T], Protocol):
+    def __getitem__(self, idx) -> T:
         ...
 
     def __len__(self) -> int:
