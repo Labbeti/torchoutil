0176504fa622bf0d9e3534606eb6edb8ee4a6527
Labbeti <etienne.labbe31@gmail.com>
2024-02-06 15:16:35 +0100

    Fix: get_auto_num_cpus func on windows and macos now return the number of CPUs to avoid an error on these OS.

--- src/torchoutil/utils/data/dataloader.py
+++ src/torchoutil/utils/data/dataloader.py
@@ -5,5 +5,14 @@ import os
 
 
 def get_auto_num_cpus() -> int:
-    """Returns the number of CPUs available, useful for setting num_workers argument in DataLoaders."""
-    return len(os.sched_getaffinity(0))
+    """Returns the number of CPUs available for several Linux-based platforms.
+
+    Useful for setting num_workers argument in DataLoaders.
+    """
+    try:
+        num_cpus = len(os.sched_getaffinity(0))
+    except AttributeError:
+        num_cpus = os.cpu_count()
+        if num_cpus is None:
+            num_cpus = 0
+    return num_cpus
