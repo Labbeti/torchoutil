fe5cab272db2dbd1ba2ea2f3e0c4a0587ddc5c9f
Labbeti <etienne.labbe31@gmail.com>
2024-02-08 11:03:00 +0100

    Add/Mod: Add load_event_files and get_duration func for tensorboard files and update typing with TensorboardEvent.

--- src/torchoutil/utils/tensorboard.py
+++ src/torchoutil/utils/tensorboard.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import glob
 import logging
 import os.path as osp
-from typing import Any, Dict, List
+from pathlib import Path
+from typing import Any, Dict, Iterable, List, TypedDict, Union
+
+from typing_extensions import NotRequired
 
 from torchoutil.utils.packaging import _TENSORBOARD_AVAILABLE
 
@@ -22,14 +26,32 @@ logger = logging.getLogger(__name__)
 _EVENT_FILE_PREFIX = "events.out.tfevents."
 _DT_FLOAT = 1
 _DT_STRING = 7
+_DTYPES = (_DT_FLOAT, _DT_STRING)
+
+
+class TensorboardEvent(TypedDict):
+    wall_time: float
+    step: int
+    tag: str
+    dtype: str
+    value: Union[str, float]
+    string_val: NotRequired[str]
+    float_val: NotRequired[List[float]]
 
 
 def load_event_file(
-    fpath: str,
+    fpath: Union[str, Path],
     cast_float_and_str: bool = True,
     ignore_underscore_tags: bool = True,
     verbose: int = 0,
-) -> List[Dict[str, Any]]:
+) -> List[TensorboardEvent]:
+    """
+    Args:
+        fpath: File path to a tensorboard event file.
+        cast_float_and_str: Cast string to floats and store result in 'value' field. defaults to True.
+        ignore_underscore_tags: Ignore event when tag starts with an underscore. defaults to True.
+        verbose: Verbose level. Higher value means more log messages. defaults to 0.
+    """
     if not osp.isfile(fpath):
         raise FileNotFoundError(f"Invalid argument {fpath=}. (not a file)")
 
@@ -69,29 +91,86 @@ def load_event_file(
                 )
             continue
 
-        if cast_float_and_str:
-            if dtype == _DT_FLOAT:
-                float_val: List[float] = data_i["float_val"]
+        if dtype == _DT_FLOAT:
+            float_val: List[float] = data_i["float_val"]
+            dtype = "float"
+
+            if cast_float_and_str:
                 assert len(float_val) == 1
                 value = float_val[0]
-                dtype = "float"
-
-            elif dtype == _DT_STRING:
-                string_val: str = data_i["string_val"]
-                value = string_val[3:-2]
-                dtype = "str"
-                tag = tag.split("/")[0]
-
+                del data_i["string_val"]
+                del data_i["float_val"]
             else:
-                raise RuntimeError(f"Unknown value {dtype=}.")
+                value = float_val
 
-            del data_i["string_val"]
-            del data_i["float_val"]
+        elif dtype == _DT_STRING:
+            string_val: str = data_i["string_val"]
+            dtype = "str"
 
-            data_i["tag"] = tag
-            data_i["dtype"] = dtype
-            data_i["value"] = value
+            if cast_float_and_str:
+                value = string_val[3:-2]
+                tag = tag.split("/")[0]
+                del data_i["string_val"]
+                del data_i["float_val"]
+            else:
+                value = string_val
+
+        else:
+            raise RuntimeError(f"Unknown value {dtype=}. (expected one of {_DTYPES})")
+
+        data_i["tag"] = tag
+        data_i["dtype"] = dtype
+        data_i["value"] = value
 
         data.append(data_i)
 
     return data
+
+
+def load_event_files(
+    paths_or_patterns: Union[str, Path, Iterable[Union[str, Path]]],
+    cast_float_and_str: bool = True,
+    ignore_underscore_tags: bool = True,
+    verbose: int = 0,
+) -> Dict[str, List[TensorboardEvent]]:
+    """
+    Args:
+        paths_or_patterns: Path or glob patterns to multiple files.
+        cast_float_and_str: Cast string to floats and store result in 'value' field. defaults to True.
+        ignore_underscore_tags: Ignore event when tag starts with an underscore. defaults to True.
+        verbose: Verbose level. Higher value means more log messages. defaults to 0.
+    """
+    if isinstance(paths_or_patterns, (str, Path)):
+        paths_or_patterns = [str(paths_or_patterns)]
+    else:
+        paths_or_patterns = [
+            str(path_or_pattern) for path_or_pattern in paths_or_patterns
+        ]
+
+    paths = [
+        path
+        for path_or_pattern in paths_or_patterns
+        for path in glob.iglob(path_or_pattern)
+    ]
+    all_events = {}
+    for path in paths:
+        events = load_event_file(
+            path,
+            cast_float_and_str,
+            ignore_underscore_tags,
+            verbose,
+        )
+        all_events[path] = events
+
+    return all_events
+
+
+def get_duration(
+    fpath: Union[str, Path],
+    verbose: int = 0,
+) -> float:
+    """Return time elapsed between first and last log in a tensorboard event file."""
+    events = load_event_file(fpath, cast_float_and_str=True, verbose=verbose)
+    wall_times = [event["wall_time"] for event in events]
+    duration = max(wall_times) - min(wall_times)
+    return duration
