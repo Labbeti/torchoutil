4d80a08c369657bcaa24dd5a2a04baf227749b02
Labbeti <etienne.labbe31@gmail.com>
2024-02-14 16:59:15 +0100

    Mod: Update version in init file.

--- src/torchoutil/__init__.py
+++ src/torchoutil/__init__.py
@@ -10,7 +10,7 @@ __author_email__ = "labbeti.pub@gmail.com"
 __license__ = "MIT"
 __maintainer__ = "Étienne Labbé (Labbeti)"
 __status__ = "Development"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 
 from .nn.functional import *
