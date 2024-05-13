e49eeca5bab166ee5b50d03d254506db9ad84ff5
LABBE Etienne <etienne.labbe@irit.fr>
2024-05-13 11:18:46 +0200

    Fix: Import order in init.

--- src/torchoutil/nn/__init__.py
+++ src/torchoutil/nn/__init__.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Redundant imports to avoid to import PyTorch modules
-from . import functional as functional
-from . import modules as modules
 from .modules import *
+
+# Redundant imports to avoid to import PyTorch modules
+from . import functional as functional  # isort:skip
+from . import modules as modules  # isort:skip
