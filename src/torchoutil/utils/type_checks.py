4a99aac8fad65c08776ac81e86346d5f5be59af3
Labbeti <etienne.labbe31@gmail.com>
2024-06-10 14:45:04 +0200

    Fix: Backward compatibility with pytorch type guards.

--- src/torchoutil/utils/type_checks.py
+++ src/torchoutil/utils/type_checks.py
@@ -2,4 +2,6 @@
 # -*- coding: utf-8 -*-
 
 # for backward compatibility
-from torchoutil.utils.stdlib.typing import *  # noqa: F401,F403  type: ignore
+from torchoutil.types.guards import *  # noqa: F401,F403  type: ignore
+from torchoutil.utils.stdlib.typing.classes import *  # noqa: F401,F403  type: ignore
+from torchoutil.utils.stdlib.typing.guards import *  # noqa: F401,F403  type: ignore
