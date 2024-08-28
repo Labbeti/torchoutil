58d3a652b53f2587364d682105fdf225f062aad7
Labbeti <etienne.labbe31@gmail.com>
2024-08-28 19:00:42 +0200

    Add: New unittests for tensor typing classes.

--- tests/test_types_tensor_typing.py
+++ tests/test_types_tensor_typing.py
@@ -1,27 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import random
 import unittest
 from unittest import TestCase
 
 import torch
 
 from torchoutil.types.tensor_typing import (
+    DoubleTensor,
     FloatTensor,
     FloatTensor0D,
+    FloatTensor1D,
     IntTensor,
     IntTensor0D,
-    Tensor,
+    LongTensor,
     Tensor0D,
     Tensor1D,
 )
 
 
 class TestTensorTyping(TestCase):
-    def test_example_1(self) -> None:
+    def test_instance_checks(self) -> None:
         x = torch.rand((), dtype=torch.float)
 
-        assert isinstance(x, Tensor)
+        assert isinstance(x, torch.Tensor)
         assert isinstance(x, Tensor0D)
         assert isinstance(x, FloatTensor)
         assert isinstance(x, FloatTensor0D)
@@ -30,12 +33,37 @@ class TestTensorTyping(TestCase):
         assert not isinstance(x, IntTensor)
         assert not isinstance(x, IntTensor0D)
 
-    def test_example_2(self) -> None:
-        assert issubclass(Tensor0D, Tensor)
-        assert issubclass(FloatTensor, Tensor)
-        assert issubclass(FloatTensor0D, Tensor)
+    def test_subclassing_checks(self) -> None:
+        assert issubclass(Tensor0D, torch.Tensor)
+        assert issubclass(FloatTensor, torch.Tensor)
+        assert issubclass(FloatTensor0D, torch.Tensor)
 
-        # note: however, torchoutil.FloatTensor != torch.FloatTensor and issubclass(torchoutil.FloatTensor, torch.FloatTensor) is False because toch.FloatTensor cannot be subclassed
+        assert issubclass(FloatTensor0D, Tensor0D)
+        assert issubclass(FloatTensor0D, FloatTensor)
+
+        assert not issubclass(FloatTensor0D, IntTensor0D)
+        assert not issubclass(FloatTensor0D, FloatTensor1D)
+
+        assert not issubclass(torch.Tensor, FloatTensor)
+        assert not issubclass(torch.Tensor, Tensor0D)
+        assert not issubclass(torch.Tensor, FloatTensor0D)
+        assert not issubclass(torch.Tensor, IntTensor)
+
+    def test_default_dtype(self) -> None:
+        x = [random.randint(0, 100) for _ in range(10)]
+
+        assert Tensor1D(x).dtype == torch.long  # torch.long is the inferred dtype for x
+        assert FloatTensor(x).dtype == torch.float
+        assert DoubleTensor(x).dtype == torch.double
+        assert LongTensor(x).dtype == torch.long
+
+    def test_torch_floattensor_compat(self) -> None:
+        x = FloatTensor()
+
+        assert isinstance(x, FloatTensor)
+        assert isinstance(x, Tensor0D)
+        assert isinstance(x, FloatTensor0D)
+        assert isinstance(x, torch.FloatTensor)
 
 
 if __name__ == "__main__":
