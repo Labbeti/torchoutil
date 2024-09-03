d2ed750e68ad41fe8a68d269071a085c5b6eb249
Labbeti <etienne.labbe31@gmail.com>
2024-09-03 13:32:15 +0200

    Add: Test with DatasetSlicer base class.

--- tests/test_utils_data_slicer.py
+++ tests/test_utils_data_slicer.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import random
 import unittest
 from typing import Union
 from unittest import TestCase
 
-from torchoutil.utils.data.slicer import DatasetSlicerWrapper
+from torchoutil.utils.data.slicer import DatasetSlicer, DatasetSlicerWrapper
 
 
 class DummySequence1:
@@ -28,32 +29,52 @@ class DummySequence2:
         return (2 * idx,) + tuple(args)
 
 
-class TestSlicer(TestCase):
-    def test_slicer_1(self) -> None:
-        dset = DummySequence1()
-        slicer = DatasetSlicerWrapper(dset)
+class DummySequence3(DatasetSlicer):
+    def __len__(self) -> int:
+        return 5
 
-        indices = list(range(len(dset)))
-        mask = [True] * len(dset)
-        items = [dset[i] for i in indices]
+    def get_item(self, idx: int) -> int:
+        return idx * 3
+
+
+class TestSlicer(TestCase):
+    def test_slicer_wrapper(self) -> None:
+        ds = DummySequence1()
+        slicer = DatasetSlicerWrapper(ds)
+
+        indices = list(range(len(ds)))
+        mask = [True] * len(ds)
+        items = [ds[i] for i in indices]
 
         assert items == slicer[:]
         assert items == slicer[indices]
         assert items == slicer[mask]
 
-    def test_slicer_2(self) -> None:
-        dset = DummySequence2()
-        slicer = DatasetSlicerWrapper(dset)
+    def test_slicer_wrapper_args(self) -> None:
+        ds = DummySequence2()
+        slicer = DatasetSlicerWrapper(ds)
 
-        indices = list(range(len(dset)))
-        mask = [True] * len(dset)
-        items = [dset[i] for i in indices]
+        indices = list(range(len(ds)))
+        mask = [True] * len(ds)
+        items = [ds[i] for i in indices]
 
         assert items == slicer[:]
         assert items == slicer[indices]
         assert items == slicer[mask]
 
         assert [(2, "a", "b"), (4, "a", "b")] == slicer[1:3, "a", "b"]
+
+    def test_slicer_base_class(self) -> None:
+        ds = DummySequence3()
+
+        indices = list(range(len(ds)))
+        random.shuffle(indices)
+
+        mask = [random.random() > 0.5 for _ in range(len(ds))]
+
+        assert ds[:] == [ds[i] for i in range(len(ds))]
+        assert ds[indices] == [ds[idx] for idx in indices]
+        assert ds[mask] == [ds[i] for i in range(len(ds)) if mask[i]]
 
 
 if __name__ == "__main__":
