92cf9571e5cf953b4499bf3961f3c7919bbc283b
Labbeti <etienne.labbe31@gmail.com>
2024-09-04 19:26:21 +0200

    Mod: Make default values public.

--- src/torchoutil/pyoutil/argparse.py
+++ src/torchoutil/pyoutil/argparse.py
@@ -3,17 +3,17 @@
 
 from typing import Iterable, List, Optional, Union
 
-_TRUE_VALUES = ("True", "t", "yes", "y", "1")
-_FALSE_VALUES = ("False", "f", "no", "n", "0")
-_NONE_VALUES = ("None", "null")
+DEFAULT_TRUE_VALUES = ("True", "t", "yes", "y", "1")
+DEFAULT_FALSE_VALUES = ("False", "f", "no", "n", "0")
+DEFAULT_NONE_VALUES = ("None", "null")
 
 
 def str_to_bool(
     x: str,
     *,
     case_sensitive: bool = False,
-    true_values: Union[str, Iterable[str]] = _TRUE_VALUES,
-    false_values: Union[str, Iterable[str]] = _FALSE_VALUES,
+    true_values: Union[str, Iterable[str]] = DEFAULT_TRUE_VALUES,
+    false_values: Union[str, Iterable[str]] = DEFAULT_FALSE_VALUES,
 ) -> bool:
     """Convert string values to bool. Intended for argparse boolean arguments.
 
@@ -22,66 +22,65 @@ def str_to_bool(
     - Other raises ValueError.
     """
     true_values = _sanitize_values(true_values)
-    false_values = _sanitize_values(false_values)
-
     if _str_in(x, true_values, case_sensitive):
         return True
-    elif _str_in(x, false_values, case_sensitive):
+
+    false_values = _sanitize_values(false_values)
+    if _str_in(x, false_values, case_sensitive):
         return False
-    else:
-        values = tuple(true_values + false_values)
-        raise ValueError(f"Invalid argument '{x}'. (expected one of {values})")
+
+    values = tuple(true_values + false_values)
+    raise ValueError(f"Invalid argument '{x}'. (expected one of {values})")
 
 
 def str_to_optional_int(
     x: str,
     *,
     case_sensitive: bool = False,
-    none_values: Union[str, Iterable[str]] = _NONE_VALUES,
+    none_values: Union[str, Iterable[str]] = DEFAULT_NONE_VALUES,
 ) -> Optional[int]:
     none_values = _sanitize_values(none_values)
-
     if _str_in(x, none_values, case_sensitive):
         return None
-    else:
-        return int(x)
+
+    return int(x)
 
 
 def str_to_optional_str(
     x: str,
     *,
     case_sensitive: bool = False,
-    none_values: Union[str, Iterable[str]] = _NONE_VALUES,
+    none_values: Union[str, Iterable[str]] = DEFAULT_NONE_VALUES,
 ) -> Optional[str]:
     none_values = _sanitize_values(none_values)
-
     if _str_in(x, none_values, case_sensitive):
         return None
-    else:
-        return x
+
+    return x
 
 
 def str_to_optional_bool(
     x: str,
     *,
     case_sensitive: bool = False,
-    true_values: Union[str, Iterable[str]] = _TRUE_VALUES,
-    false_values: Union[str, Iterable[str]] = _FALSE_VALUES,
-    none_values: Union[str, Iterable[str]] = _NONE_VALUES,
+    true_values: Union[str, Iterable[str]] = DEFAULT_TRUE_VALUES,
+    false_values: Union[str, Iterable[str]] = DEFAULT_FALSE_VALUES,
+    none_values: Union[str, Iterable[str]] = DEFAULT_NONE_VALUES,
 ) -> Optional[bool]:
     true_values = _sanitize_values(true_values)
-    false_values = _sanitize_values(false_values)
-    none_values = _sanitize_values(none_values)
-
     if _str_in(x, true_values, case_sensitive):
         return True
-    elif _str_in(x, false_values, case_sensitive):
+
+    false_values = _sanitize_values(false_values)
+    if _str_in(x, false_values, case_sensitive):
         return False
-    elif _str_in(x, none_values, case_sensitive):
+
+    none_values = _sanitize_values(none_values)
+    if _str_in(x, none_values, case_sensitive):
         return None
-    else:
-        values = tuple(true_values + false_values + none_values)
-        raise ValueError(f"Invalid argument '{x}'. (expected one of {values})")
+
+    values = tuple(true_values + false_values + none_values)
+    raise ValueError(f"Invalid argument '{x}'. (expected one of {values})")
 
 
 def _sanitize_values(values: Union[str, Iterable[str]]) -> List[str]:
