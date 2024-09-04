23c17dc1fbd36703a821bd7a30674f61e09c0029
Labbeti <etienne.labbe31@gmail.com>
2024-09-04 10:53:44 +0200

    Fix: Internal typing of return_types.

--- src/torchoutil/utils/return_types.py
+++ src/torchoutil/utils/return_types.py
@@ -34,8 +34,8 @@ if sys.version_info.major == 3 and sys.version_info.minor <= 8:
         valid: bool
         shape: T  # type: ignore
 
-    class shape(_shape_base, Generic[T]):
-        ...  # type: ignore
+    class shape(_shape_base, Generic[T]):  # type: ignore
+        ...
 
 else:
 
