295436a614eefec3dd46a68b9a2db1d0fd66fccf
Labbeti <etienne.labbe31@gmail.com>
2024-09-11 15:25:17 +0200

    Add: ExistsMode in init.

--- src/torchoutil/utils/pack/__init__.py
+++ src/torchoutil/utils/pack/__init__.py
@@ -3,6 +3,7 @@
 
 from torchoutil.utils.pack.dataset import PackedDataset
 from torchoutil.utils.pack.pack import (
+    ExistsMode,
     pack_dataset,
     pack_dataset_dict,
     pack_dataset_per_column,
