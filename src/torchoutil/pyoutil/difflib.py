5bc22ccb95232ed0d0de9b517a88c5b433a07171
Labbeti <etienne.labbe31@gmail.com>
2024-11-14 16:39:27 +0100

    Mod: Update find_closest_in_list fn to accept iterable of strings.

--- src/torchoutil/pyoutil/difflib.py
+++ src/torchoutil/pyoutil/difflib.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import math
 from difflib import SequenceMatcher
-from typing import Callable, Sequence
-
-from .collections import argmax
+from typing import Callable, Iterable
 
 
 def sequence_matcher_ratio(a: str, b: str) -> float:
@@ -13,15 +12,22 @@ def sequence_matcher_ratio(a: str, b: str) -> float:
 
 def find_closest_in_list(
     x: str,
-    lst: Sequence[str],
+    lst: Iterable[str],
     sim_fn: Callable[[str, str], float] = sequence_matcher_ratio,
+    higher_is_closer: bool = True,
 ) -> str:
-    if len(lst) <= 0:
-        msg = f"Invalid argument {lst=}. (expected non-empty sequence of strings)"
-        raise ValueError(msg)
+    best_sim = -int(higher_is_closer) * math.inf
 
-    ratios = [sim_fn(x, expected_i) for expected_i in lst]
-    idx = argmax(ratios)
-    closest = lst[idx]
+    for elt in lst:
+        sim = sim_fn(x, elt)
+        if (higher_is_closer and best_sim < sim) or (
+            not higher_is_closer and best_sim > sim
+        ):
+            best_sim = sim
+            closest = elt
+
+    if math.isinf(best_sim):
+        msg = f"Invalid argument {lst=}. (expected non-empty iterable of strings)"
+        raise ValueError(msg)
 
     return closest
