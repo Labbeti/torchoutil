4d5136b948994e2774ebdac94d2593f53b8aa9b0
Labbeti <labbeti.pub@gmail.com>
2025-03-21 00:42:22 +0100

    Fix: Try to fix torchoutil version hash.

--- src/torchoutil/__init__.py
+++ src/torchoutil/__init__.py
@@ -3,7 +3,18 @@
 
 """Collection of functions and modules to help development in PyTorch."""
 
-from .core.version import get_githash_short
+import subprocess
+
+
+def get_githash_short() -> str:
+    cmd = ["git", "rev-parse", "--short", "HEAD"]
+    return subprocess.check_output(cmd).decode().strip()
+
+
+def get_githash_full() -> str:
+    cmd = ["git", "rev-parse", "HEAD"]
+    return subprocess.check_output(cmd).decode().strip()
+
 
 __name__ = "torchoutil"
 __author__ = "Étienne Labbé (Labbeti)"
